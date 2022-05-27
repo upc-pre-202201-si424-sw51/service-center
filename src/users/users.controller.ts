f20dfd643309e7db6f9e350842b6d4a7dec40727
Angel Velasquez <pcisavel@upc.edu.pe>
2022-05-27 12:55:14 -0500

    feat(users): Added Inbound service layer methods.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from "@nestjs/common";
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  async getAllUsers() {
+    const users = await this.usersService.getAll();
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Users fetched successfully',
+      users,
+    };
+  }
+}
